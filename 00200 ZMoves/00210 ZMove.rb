aa2f51b4ebf3f37e0aa41e607bfb2be187ce8f05
Aelysya <93989677+Aelysya@users.noreply.github.com>
2025-02-17 20:16:46 +0100

    change power calc to take into account power-varying moves

--- 00200 ZMoves/00210 ZMove.rb
+++ 00200 ZMoves/00210 ZMove.rb
@@ -2,12 +2,9 @@ module Battle
   class Move
     # Class managing type-specific Z-Moves
     class ZMove < Basic
-      # Power of the original move linked to this Z-Move
+      # Original move linked to this Z-Move
       # @return [Integer]
-      attr_reader :original_move_power
-      # db_symbol of the original move linked to this Z-Move
-      # @return [Integer]
-      attr_reader :original_move_db_symbol
+      attr_reader :original_move
 
       # Create a new move
       # @param db_symbol [Symbol] db_symbol of the move in the database
@@ -16,19 +13,24 @@ module Battle
       # @param scene [Battle::Scene] current battle scene
       # @param original_move [Battle::Move] original move linked to this Z-Move
       def initialize(db_symbol, scene, original_move)
-        @original_move_power = data_move(original_move.db_symbol).power
-        @original_move_db_symbol = original_move.db_symbol
+        @original_move = original_move
+        original_move.real_base_power
         super(db_symbol, 1, 1, scene)
       end
 
       # List of exceptions for the power calculation
       Z_MOVES_POWER_EXCEPTIONS = {
+        struggle: 1,
         mega_drain: 120,
         weather_ball: 160,
         hex: 160,
         v_create: 220,
         flying_press: 170,
         core_enforcer: 140,
+        fissure: 180,
+        guillotine: 180,
+        horn_drill: 180,
+        sheer_cold: 180,
       }
 
       # Get the real base power of the move (taking in account all parameter)
@@ -37,21 +39,23 @@ module Battle
       # @param target [PFM::PokemonBattler] target of the move
       # @return [Integer]
       def real_base_power(user, target)
-        # @original_move_power <= 55
+        original_power = @original_move.real_base_power(user, target)
+
+        # @original_move_power < 60
         power = 100
 
-        if Z_MOVES_POWER_EXCEPTIONS.key?(@original_move_db_symbol)
-          power = Z_MOVES_POWER_EXCEPTIONS[@original_move_db_symbol]
+        if Z_MOVES_POWER_EXCEPTIONS.key?(@original_move.db_symbol)
+          power = Z_MOVES_POWER_EXCEPTIONS[@original_move.db_symbol]
         else
-          power = 120 if @original_move_power == 60  || @original_move_power == 65
-          power = 140 if @original_move_power == 70  || @original_move_power == 75
-          power = 160 if @original_move_power == 80  || @original_move_power == 85
-          power = 175 if @original_move_power == 90  || @original_move_power == 95
-          power = 180 if @original_move_power == 100 || @original_move_power == 105
-          power = 185 if @original_move_power == 110 || @original_move_power == 115
-          power = 190 if @original_move_power == 120 || @original_move_power == 125
-          power = 195 if @original_move_power == 130 || @original_move_power == 135
-          power = 200 if @original_move_power >= 140
+          power = 120 if original_power >= 60  && original_power <= 69
+          power = 140 if original_power >= 70  && original_power <= 79
+          power = 160 if original_power >= 80  && original_power <= 89
+          power = 175 if original_power >= 90  && original_power <= 99
+          power = 180 if original_power >= 100 && original_power <= 109
+          power = 185 if original_power >= 110 && original_power <= 119
+          power = 190 if original_power >= 120 && original_power <= 129
+          power = 195 if original_power >= 130 && original_power <= 139
+          power = 200 if original_power >= 140
         end
         log_data("power = #{power} # after #{self.class} real_base_power")
 
