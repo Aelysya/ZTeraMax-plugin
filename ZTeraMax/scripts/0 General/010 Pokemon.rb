2aa1a760f3a4c5f48c9f8154ea23d8e17a123f10
Aelysya <93989677+Aelysya@users.noreply.github.com>
2025-03-04 22:09:16 +0100

    add basic switch anim for dynamax

--- ZTeraMax/scripts/0 General/010 Pokemon.rb
+++ ZTeraMax/scripts/0 General/010 Pokemon.rb
@@ -3,6 +3,12 @@ module PFM
     # Reset the Pokémon's moveset to its original state
     # @param pokemon [Pokemon] The Pokémon whose moveset is to be updated.
     def reset_to_original_moveset
+      effects.each do |effect|
+        next unless effect.name == :z_power # || effect.name == :dynamaxed
+
+        effect.kill
+      end
+
       original_moveset.each_with_index do |move, i|
         moveset[i] = Battle::Move[move.be_method].new(move.db_symbol, move.pp, move.ppmax, @scene)
       end
--- ZTeraMax/scripts/2 Dynamax/008 Effects.rb
+++ ZTeraMax/scripts/2 Dynamax/008 Effects.rb
@@ -17,7 +17,6 @@ module Battle
 
       # Function called when the effect has been deleted from the effects handler
       def on_delete
-        # @logic.scene.display_message_and_wait(parse_text_with_pokemon(20_000, XX, @pokemon))
         @pokemon.reset_to_original_moveset
       end
     end
