8885557f5940a350e495a5f8683deb3ec83f9225
Aelysya <93989677+Aelysya@users.noreply.github.com>
2025-03-08 23:15:22 +0100

    Fix moveset reset on death and switch out

--- ZTeraMax/scripts/2 Dynamax/008 Effects.rb
+++ ZTeraMax/scripts/2 Dynamax/008 Effects.rb
@@ -19,6 +19,21 @@ module Battle
       def on_delete
         @pokemon.reset_to_original_moveset
       end
+
+      # Function called when the effect has been deleted from the effects handler
+      def on_switch_event(handler, who, with)
+        kill
+      end
+
+      # Function called after damages were applied and when target died (post_damage_death)
+      # @param handler [Battle::Logic::DamageHandler]
+      # @param hp [Integer] number of hp (damage) dealt
+      # @param target [PFM::PokemonBattler]
+      # @param launcher [PFM::PokemonBattler, nil] Potential launcher of a move
+      # @param skill [Battle::Move, nil] Potential move used
+      def on_post_damage_death(handler, hp, target, launcher, skill)
+        kill
+      end
     end
 
     class MaxGuard < Protect
