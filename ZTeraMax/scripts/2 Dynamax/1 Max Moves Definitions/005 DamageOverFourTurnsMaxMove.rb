af547f7ff9e7adf7e4b705a75906ff19a03e7b86
Aelysya <93989677+Aelysya@users.noreply.github.com>
2025-03-09 19:48:15 +0100

    add new GMax effects

---
+++ ZTeraMax/scripts/2 Dynamax/1 Max Moves Definitions/005 DamageOverFourTurnsMaxMove.rb
@@ -0,0 +1,29 @@
+module Battle
+  class Move
+    class DamageOverFourTurnsMaxMove < MaxMove
+      # Test if the effect is working
+      # @param user [PFM::PokemonBattler] user of the move
+      # @param actual_targets [Array<PFM::PokemonBattler>] targets that will be affected by the move
+      # @return [Boolean]
+      def effect_working?(user, actual_targets)
+        !actual_targets.all? { |target|
+          logic.bank_effects[target.bank].has?(:damage_over_four_turns) &&
+            logic.bank_effects[target.bank].get(:damage_over_four_turns).position == target.position
+        }
+      end
+
+      # Function that deals the effect to the pokemon
+      # @param user [PFM::PokemonBattler] user of the move
+      # @param actual_targets [Array<PFM::PokemonBattler>] targets that will be affected by the move
+      def deal_effect(user, actual_targets)
+        @logic.foes_of(user).each do |target|
+          next if logic.bank_effects[target.bank].has?(:damage_over_four_turns) &&
+                  logic.bank_effects[target.bank].get(:damage_over_four_turns).position == target.position
+
+          logic.bank_effects[target.bank].add(Battle::Effects::DamageOverFourTurns.new(logic, target.bank, target.position, self))
+        end
+      end
+    end
+    Move.register(:s_damage_over_four_turns_max_move, DamageOverFourTurnsMaxMove)
+  end
+end
---
+++ ZTeraMax/scripts/2 Dynamax/1 Max Moves Definitions/008 GMaxResonance.rb
@@ -0,0 +1,17 @@
+module Battle
+  class Move
+    class GMaxResonance < MaxMove
+      private
+
+      # Function that deals the effect to the pokemon
+      # @param user [PFM::PokemonBattler] user of the move
+      # @param actual_targets [Array<PFM::PokemonBattler>] targets that will be affected by the move
+      def deal_effect(user, actual_targets)
+        turn_count = user.hold_item?(:light_clay) ? 8 : 5
+        logic.bank_effects[user.bank].add(Effects::AuroraVeil.new(logic, user.bank, 0, turn_count))
+        # scene.display_message_and_wait(parse_text(18, 288 + user.bank.clamp(0, 1))) # Ligne vide dans le CSV, à corriger sur PSDK
+      end
+    end
+    Move.register(:s_gmax_resonance, GMaxResonance)
+  end
+end
