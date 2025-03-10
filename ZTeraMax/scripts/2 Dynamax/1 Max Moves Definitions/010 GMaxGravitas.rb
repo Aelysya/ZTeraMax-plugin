d09dbaf96d4f97483970becdeba27a25e09936fb
Aelysya <93989677+Aelysya@users.noreply.github.com>
2025-03-10 16:03:55 +0100

    fix GMaxGravitas

--- ZTeraMax/scripts/2 Dynamax/1 Max Moves Definitions/010 GMaxGravitas.rb
+++ ZTeraMax/scripts/2 Dynamax/1 Max Moves Definitions/010 GMaxGravitas.rb
@@ -3,18 +3,6 @@ module Battle
     class GMaxGravitas < MaxMove
       private
 
-      # Function that tests if the user is able to use the move
-      # @param user [PFM::PokemonBattler] user of the move
-      # @param targets [Array<PFM::PokemonBattler>] expected targets
-      # @note Thing that prevents the move from being used should be defined by :move_prevention_user Hook
-      # @return [Boolean] if the procedure can continue
-      def move_usable_by_user(user, targets)
-        return false unless super
-        return show_usage_failure(user) && false if @logic.terrain_effects.has?(:gravity)
-
-        return true
-      end
-
       # Function that deals the effect to the pokemon
       # @param user [PFM::PokemonBattler] user of the move
       # @param actual_targets [Array<PFM::PokemonBattler>] targets that will be affected by the move
