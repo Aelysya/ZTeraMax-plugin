b048d11f98819c06cdb69afd37a55911d8272289
Aelysya <93989677+Aelysya@users.noreply.github.com>
2025-03-18 20:05:24 +0100

    add unseen fist management to max guard

--- ZTeraMax/scripts/2 Dynamax/2 Effects/006 MaxGuard.rb
+++ ZTeraMax/scripts/2 Dynamax/2 Effects/006 MaxGuard.rb
@@ -8,6 +8,7 @@ module Battle
       # @return [Boolean] if the target is evading the move
       def on_move_prevention_target(user, target, move)
         if trampling_move?(move) # Override the protect effect allowing trampling moves to go through
+          return false if user.has_ability?(:unseen_fist) && move.direct?
           return false if move.db_symbol == :gmax_one_blow || move.db_symbol == :gmax_rapid_flow
 
           play_protect_effect(user, target, move)
